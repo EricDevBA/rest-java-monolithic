9534e3d92724506c58b6d94cdd1d7e378c661d14
Eric Rosário <ericsantos.r@hotmail.com>
2021-03-17 11:45:08 -0300

    Changes Import Package and Class

--- src/main/java/com/ericrosarioaluno/demo/controller/InstalacaoController.java
+++ src/main/java/com/ericrosarioaluno/demo/controller/InstalacaoController.java
@@ -16,11 +16,11 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.nomealuno.demoacmeap.domain.Cliente;
-import com.nomealuno.demoacmeap.domain.Instalacao;
-import com.nomealuno.demoacmeap.exception.RecursoNotFoundException;
-import com.nomealuno.demoacmeap.repository.ClienteRepository;
-import com.nomealuno.demoacmeap.repository.InstalacaoRepository;
+import com.ericrosarioaluno.demo.domain.Cliente;
+import com.ericrosarioaluno.demo.domain.Instalacao;
+import com.ericrosarioaluno.demo.exception.RecursoNotFoundException;
+import com.ericrosarioaluno.demo.repository.ClienteRepository;
+import com.ericrosarioaluno.demo.repository.InstalacaoRepository;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
